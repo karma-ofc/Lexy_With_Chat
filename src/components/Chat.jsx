671c0a7e8f0bde07ff3bbb79e968c57a73c8db70
karma-ofc <sleepwalkermrg@gmail.com>
2026-06-09 23:13:35 +0300

    Add Chat component (placeholder)

---
+++ src/components/Chat.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Chat() {
+  return (
+    <div>
+      <h2>Chat Component (placeholder)</h2>
+      <p>Real-time chat UI will be here.</p>
+    </div>
+  );
+}
